8651dcd3049c9fd73553a9fdec31d0a3cb15c4c4
JAGUIAR.S <66434808+josedoce@users.noreply.github.com>
2022-12-02 21:40:18 +0000

    splashscreen delayed

--- app/src/main/java/github/josedoce/anotador/views/MainActivity.java
+++ app/src/main/java/github/josedoce/anotador/views/MainActivity.java
@@ -13,41 +13,27 @@ import github.josedoce.anotador.database.DBHelper;
 import github.josedoce.anotador.database.DBUser;
 
 public class MainActivity extends AppCompatActivity {
-
+    private final DBUser dbUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.splash_screen_layout);
-        SplashScreen.handler(new SplashScreen(this));
-    }
-
-    private static class SplashScreen implements Runnable {
-        private final DBUser dbUser;
-        public static void handler(SplashScreen splashScreen){
-            new Handler().postDelayed(splashScreen, 3000);
-        }
-
-        Context ctx;
-        public SplashScreen(Context ctx){
-            this.ctx = ctx;
-            DBHelper db = new DBHelper(ctx);
-            dbUser = new DBUser(db);
-        }
-
-        @Override
-        public void run() {
+    
+        DBHelper db = new DBHelper(this);
+        this.dbUser = new DBUser(db);
+        Intent intent;
+        //modificação no postDelayed
+        new Handler().postDelayed(()->{
             Cursor c = this.dbUser.selectAll();
             c.moveToFirst();
-            Intent intent;
-
+            
             if(c.getCount() > 0){
-                intent = new Intent(ctx, SignInActivity.class);
+                intent = new Intent(this, SignInActivity.class);
             }else{
-                intent = new Intent(ctx, SignUpActivity.class);
+                intent = new Intent(this, SignUpActivity.class);
             }
-            this.ctx.startActivity(intent);
-        }
-
+            this.startActivity(intent);
+        }, 3000);
     }
 }
